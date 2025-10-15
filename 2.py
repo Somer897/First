0aabfd7915d6125c366c15e04c0dff186bcef1b6
Somer897 <tyme897@gmail.com>
2025-10-15 11:46:27 +0300

    qwe

--- 2.py
+++ 2.py
@@ -1,2 +1,18 @@
-print('ojegivk' \
-'97ovjevmoem')
+import numpy as np
+import pandas as pd
+import random
+r = {}
+for i in range(1, 3):
+    name = input('Имя: ')
+    sname = input('Фамилия: ')
+    pname = input('Отчество: ')
+    date = input('Дата рождения:')
+    id = (random.randint(1000, 9999))
+    n = {'Имя': name,
+        'Фамилия': sname,
+        'Отчество': pname,
+        'Дата рождения': date}
+    r.update({id:n})
+
+df = pd.DataFrame(r.values(), index=r.keys())
+print(df)              #DataFrame
